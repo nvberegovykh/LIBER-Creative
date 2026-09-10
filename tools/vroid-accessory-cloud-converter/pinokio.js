6513aa98bb8746c20cb124d8be627796552d8e06
Nikita Beregovykh <116811305+nvberegovykh@users.noreply.github.com>
2026-09-10 17:50:17 -0400

    Launch VRoid converter through localhost in Pinokio

--- tools/vroid-accessory-cloud-converter/pinokio.js
+++ tools/vroid-accessory-cloud-converter/pinokio.js
@@ -1,10 +1,13 @@
 module.exports = {
   version: "4.0",
   title: "VRoid 2.14 Accessory Converter",
-  description: "Zero-install local shell with private clean-room cloud conversion.",
-  menu: async () => [{
-    icon: "fa-solid fa-cloud-arrow-up",
-    text: "Open Converter",
-    href: "index.html"
-  }]
+  description: "Zero-install localhost shell with private clean-room cloud conversion.",
+  menu: async () => [
+    {
+      default: true,
+      icon: "fa-solid fa-cloud-arrow-up",
+      text: "Start Converter",
+      href: "start.js"
+    }
+  ]
 }
